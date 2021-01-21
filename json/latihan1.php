d2cd0d8a04b1e2f128a3eb87dd21ba14c39e47c0
Miftavinda <miftavinda@gmail.com>
2021-01-21 14:10:53 +0700

    first commit

---
+++ json/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "Mifta",
+        "nim" => "43116117",
+        "email" => "miftavinda@gmail.com"
+    ],
+    [
+        "nama" => "Mifta",
+        "nim" => "43116117",
+        "email" => "miftavinda@gmail.com"
+    ]
+];
+$data = json_encode($mahasiswa);
+echo $data;
+
+
+?> 
